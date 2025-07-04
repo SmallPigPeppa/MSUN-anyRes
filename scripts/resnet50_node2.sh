1d33797c8ac07ff84fc430620216062921d6041a
lwz <905328134@qq.com>
2025-07-05 00:34:45 +0800

    initial

--- scripts/resnet50_node2.sh
+++ scripts/resnet50_node2.sh
@@ -13,6 +13,7 @@ python3 msun/resnet50.py fit \
   --model.unified_res 56 \
   --model.alpha 1.0 \
   --trainer.max_epochs 80 \
+  --trainer.num_nodes 2 \
   --trainer.precision bf16-mixed \
   --trainer.accelerator gpu \
   --trainer.logger WandbLogger \
