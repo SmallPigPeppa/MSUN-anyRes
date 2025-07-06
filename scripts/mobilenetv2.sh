3c0c13288e568d0d18df31b147db9f0f3b31d0dc
lwz <905328134@qq.com>
2025-07-07 02:50:13 +0800

    initial

--- scripts/mobilenetv2.sh
+++ scripts/mobilenetv2.sh
@@ -9,7 +9,7 @@ python3 msun/mobilenetv2.py fit \
   --data.img_size 224 \
   --model.num_classes 1000 \
   --model.learning_rate 1e-3 \
-  --model.weight_decay 2e-4 \
+  --model.weight_decay 1e-4 \
   --model.alpha 1.0 \
   --trainer.max_epochs 90 \
   --trainer.precision bf16-mixed \
--- scripts/vgg16.sh
+++ scripts/vgg16.sh
@@ -9,7 +9,7 @@ python3 msun/vgg16.py fit \
   --data.img_size 224 \
   --model.num_classes 1000 \
   --model.learning_rate 1e-3 \
-  --model.weight_decay 2e-5 \
+  --model.weight_decay 1e-4 \
   --model.alpha 1.0 \
   --trainer.max_epochs 90 \
   --trainer.precision bf16-mixed \
