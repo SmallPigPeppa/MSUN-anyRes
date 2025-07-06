3e722759ee178837db6cecbb8a999387b6c83126
lwz <905328134@qq.com>
2025-07-06 23:53:17 +0800

    initial

--- scripts/mobilenetv2.sh
+++ scripts/mobilenetv2.sh
@@ -2,7 +2,7 @@
 # Explicit startup script for training MultiScaleResNet via LightningCLI
 
 # Example: modify values as needed
-python3 msun/mobilenetv2.py fit \
+python3 msun/mobilenetv2v2.py fit \
   --data.data_dir ./imagenet \
   --data.batch_size 256 \
   --data.num_workers 16 \
