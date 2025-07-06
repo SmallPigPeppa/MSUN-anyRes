d8fc4e22c2c3a70faee731fd124ee1e9a399714f
lwz <905328134@qq.com>
2025-07-07 03:54:03 +0800

    initial

--- scripts_sgd/vgg162.sh
+++ scripts_sgd/vgg162.sh
@@ -8,7 +8,7 @@ python3 msun/vgg16.py fit \
   --data.num_workers 16 \
   --data.img_size 224 \
   --model.num_classes 1000 \
-  --model.learning_rate 0.1 \
+  --model.learning_rate 1e-3 \
   --model.weight_decay 2e-5 \
   --model.alpha 1.0 \
   --trainer.max_epochs 90 \
