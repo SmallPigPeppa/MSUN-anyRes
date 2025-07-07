647b858fd2b89d52c3e9ccddb54c00a0f90098a6
lwz <905328134@qq.com>
2025-07-07 11:33:42 +0800

    initial

---
+++ scripts_sgd_npu/vgg16.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Explicit startup script for training MultiScaleResNet via LightningCLI
+
+# Example: modify values as needed
+#  --trainer.precision bf16-mixed \
+python3 msun_sgd/vgg16.py fit \
+  --data.data_dir ./imagenet \
+  --data.batch_size 32 \
+  --data.num_workers 16 \
+  --data.img_size 224 \
+  --model.num_classes 1000 \
+  --model.learning_rate 0.1 \
+  --model.weight_decay 2e-5 \
+  --model.alpha 0. \
+  --trainer.max_epochs 90 \
+  --trainer.precision 16 \
+  --trainer.accelerator npu \
+  --trainer.logger WandbLogger \
+  --trainer.logger.project msun-anyres \
+  --trainer.logger.name msun-vgg16 \
+  --trainer.logger.log_model False \
+  --trainer.logger.offline False \
+  --model_checkpoint.dirpath /mnt/bn/liuwenzhuo-hl-data/ckpt/msun/msun/vgg16 \
+  --model_checkpoint.monitor val/acc224 \
+  --model_checkpoint.save_top_k 1 \
+  --model_checkpoint.save_last True \
+  --lr_monitor.logging_interval epoch
+
