ea9e425cdf220145c76a7cf418543a34edd5181e
lwz <905328134@qq.com>
2025-07-07 15:13:55 +0800

    initial

--- debug/train_sgd.sh
+++ debug/train_sgd.sh
@@ -9,7 +9,7 @@ models=(resnet50)
 # 2) keep your dict of hyperparams
 declare -A params=(
 #  [resnet50]="128:0.5:2e-5:90"
-  [resnet50]="256:1.0:2e-5:90"
+  [resnet50]="128:0.5:2e-5:90"
   [densenet121]="32:0.1:2e-5:90"
 #  [vgg16]="32:0.1:2e-5:90"
   [vgg16]="256:0.4:2e-5:90"
