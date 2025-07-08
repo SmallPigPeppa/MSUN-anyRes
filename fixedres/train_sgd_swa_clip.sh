940c99df9c303ac45a3dfb69498d3b65a8c2cb17
lwz <905328134@qq.com>
2025-07-08 16:13:51 +0800

    initial

--- fixedres/train_sgd_swa_clip.sh
+++ fixedres/train_sgd_swa_clip.sh
@@ -3,14 +3,15 @@ set -euo pipefail
 
 # 1) define the exact order you want
 models=(resnet50 densenet121 vgg16 mobilenetv2)
-models=(densenet121)
+models=(vgg16)
 
 # 2) keep your dict of hyperparams
 declare -A params=(
   [resnet50]="128:0.5:2e-5:90"
 #  [densenet121]="32:0.1:2e-5:90"
   [densenet121]="128:0.5:2e-5:90"
-  [vgg16]="256:0.4:2e-5:90"
+#  [vgg16]="256:0.4:2e-5:90"
+  [vgg16]="128:0.5:2e-5:90"
   [mobilenetv2]="32:0.1:2e-5:300"
 )
 
