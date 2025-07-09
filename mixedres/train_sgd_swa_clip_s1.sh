161eb3567574b0895723eebbabccc301799f6a30
lwz <905328134@qq.com>
2025-07-10 01:10:59 +0800

    initial

---
+++ mixedres/train_sgd_swa_clip_s1.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 1) define the model order
+models=(resnet50 densenet121 vgg16 mobilenetv2)
+models=(vgg16)
+
+# 2) define dict of hyperparams: bs, lr, wd, epochs,  alpha
+
+declare -A params=(
+  [resnet50]="128:0.5:2e-5:90:0.2"
+  [densenet121]="32:0.1:2e-5:90:0.2"
+  [vgg16]="256:0.4:2e-5:90:0.2"
+  [mobilenetv2]="32:0.1:2e-5:300:0.2"
+)
+
+# 3) iterate over the *ordered* list
+for model in "${models[@]}"; do
+  # now read batch_size, lr, weight_decay, epochs, and alpha
+  IFS=':' read -r bs lr wd ep alpha <<<"${params[$model]}"
+  echo "Training $model (batch_size=$bs, lr=$lr, weight_decay=$wd, epochs=$ep, alpha=$alpha)"
+  swa_epoch_start=$(awk -v e="$ep" 'BEGIN { printf "%.6f", (e-18)/e }')
+
+  python3 mixedres/main_sgd_swa_s1.py fit \
+    --data.data_dir ./imagenet \
+    --data.batch_size "$bs" \
+    --data.num_workers 16 \
+    --data.img_size 224 \
+    --model.num_classes 1000 \
+    --model.learning_rate "$lr" \
+    --model.weight_decay "$wd" \
+    --model.model_name "$model" \
+    --trainer.max_epochs "$ep" \
+    --trainer.precision bf16-mixed \
+    --trainer.accelerator gpu \
+    --trainer.logger WandbLogger \
+    --trainer.logger.project mixedres-anyres \
+    --trainer.logger.name "mixedres-s1-$model" \
+    --trainer.logger.log_model False \
+    --trainer.logger.offline False \
+    --trainer.gradient_clip_val 0.5 \
+    --swa.swa_lrs 1e-2 \
+    --swa.swa_epoch_start "$swa_epoch_start" \
+    --model_checkpoint.dirpath "/mnt/bn/liuwenzhuo-lf/ckpt/msun/mixedres-s1/$model" \
+    --model_checkpoint.filename "epoch-{epoch:02d}-val_acc224-{val/acc224:.4f}" \
+    --model_checkpoint.auto_insert_metric_name False \
+    --model_checkpoint.monitor val/acc224 \
+    --model_checkpoint.save_top_k 1 \
+    --model_checkpoint.mode max \
+    --model_checkpoint.save_last True \
+    --lr_monitor.logging_interval epoch
+done
