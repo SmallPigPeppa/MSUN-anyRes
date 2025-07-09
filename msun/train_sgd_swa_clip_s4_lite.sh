173113fc9168b180e8f68304dd1dfbe8402af6df
lwz <905328134@qq.com>
2025-07-09 23:28:30 +0800

    initial

--- msun/train_sgd_swa_clip_s4_lite.sh
+++ msun/train_sgd_swa_clip_s4_lite.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # 1) define the model order
 models=(resnet50 densenet121 vgg16 mobilenetv2)
-models=(resnet50)
+models=(vgg16)
 
 # 2) define dict of hyperparams: bs, lr, wd, epochs,  alpha
 
