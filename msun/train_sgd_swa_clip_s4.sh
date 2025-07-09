f55a30aa5a1d3cf059bffe060b09c87269f32832
lwz <905328134@qq.com>
2025-07-10 00:27:04 +0800

    initial

--- msun/train_sgd_swa_clip_s4.sh
+++ msun/train_sgd_swa_clip_s4.sh
@@ -42,7 +42,7 @@ for model in "${models[@]}"; do
     --trainer.gradient_clip_val 0.5 \
     --swa.swa_lrs 1e-2 \
     --swa.swa_epoch_start "$swa_epoch_start" \
-    --model_checkpoint.dirpath "/mnt/bn/liuwenzhuo-lf/ckpt/msun/msun-swa-clip-s4/$model" \
+    --model_checkpoint.dirpath "/mnt/bn/liuwenzhuo-hl-data/ckpt/msun/msun-swa-clip-s4/$model" \
     --model_checkpoint.filename "epoch-{epoch:02d}-val_acc224-{val/acc224:.4f}" \
     --model_checkpoint.auto_insert_metric_name False \
     --model_checkpoint.monitor val/acc224 \
